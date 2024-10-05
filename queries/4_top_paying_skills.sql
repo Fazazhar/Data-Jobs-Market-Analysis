309d4deb3c03c93f8c825bfdef665182b2fdf771
Fazazhar <pazhar@ucsd.edu>
2024-10-05 15:37:11 -0700

    finish query for top_paying_skills and exporting datasets

--- queries/4_top_paying_skills.sql
+++ queries/4_top_paying_skills.sql
@@ -1,37 +1,38 @@
 /*
-Question 3: What are the most in-demand skills for Data Scientist?
-- Join job postings to inner join table in query 2
-- Identify the top 7 in-demand skills for Data Scientist
-- Filtered on all job postings
-- Purpose: To retrieve the top 7 skills that is currently in demand within the job market
+Question 4: What are the top skills based on salary?
+- Analyze the average salary associated with a particular skill
+- Filtering on job roles with a specified salaries, regardless of job location
+- Purpose: Understand how different skills impact salary levels for Data Scientist
 */
 
 SELECT
     skills,
-    COUNT(skills_job_dim.job_id) AS demand_count
+    ROUND(AVG(salary_year_avg), 0) as avg_salary
 FROM job_postings_fact
 INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
     job_title_short = 'Data Scientist'
+    AND salary_year_avg IS NOT NULL
 GROUP BY
     1
 ORDER BY
     2 DESC
-LIMIT 7
+LIMIT 25
 
 -- same query for 'Data Analyst' roles
 
 SELECT
     skills,
-    COUNT(skills_job_dim.job_id) AS demand_count
+    ROUND(AVG(salary_year_avg), 0) as avg_salary
 FROM job_postings_fact
 INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
-    job_title_short = 'Data Scientist'
+    job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
 GROUP BY
     1
 ORDER BY
     2 DESC
-LIMIT 7
+LIMIT 25
